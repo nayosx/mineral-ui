7d1c549e985dde333e6b5661bd46e6fafaac2304
kylegach <kyle.gach@ca.com>
2018-02-09 10:53:15 -0700

    WIP: Remove rwd from styles

--- src/styles/index.js
+++ src/styles/index.js
@@ -17,5 +17,4 @@
 /* @flow */
 export { default as createStyledComponent } from './createStyledComponent';
 export { default as getNormalizedValue } from './getNormalizedValue';
-export { default as getResponsiveStyles } from './getResponsiveStyles';
 export { default as pxToEm } from './pxToEm';
